1e94be0b899884bc82e62e3e3aca7fd6348388bf
sankamille <luc.brulet@epitech.eu>
2018-11-21 16:51:01 +0100

    [+] Mongoose method compare password added

--- models/usersModels.js
+++ models/usersModels.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose")
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-}, {timestamps: true});
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err)
+            return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 var User = mongoose.model('User', UserSchema);
 
