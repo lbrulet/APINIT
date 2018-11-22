8df87880c8d587f6e6d1df7b6ace4c9f58fd8c81
sankamille <luc.brulet@epitech.eu>
2018-11-22 07:21:02 +0100

    [+] models modified with some method added like comparePassword/preSave to hash a new password

--- models/usersModels.js
+++ models/usersModels.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const SALT_WORK_FACTOR = 10
 
 var UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
@@ -12,6 +14,22 @@ UserSchema.methods.comparePassword = function (candidatePassword, cb) {
         cb(null, isMatch);
     });
 };
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (!user.isModified('password')) 
+        return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err)
+            return next(err);
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err)
+                return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
 
 var User = mongoose.model('User', UserSchema);
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser")
+const mongoose = require('./mongo')
+const User = require("./models/usersModels")
 const app = express();
 
-const PORT = 5000
+const PORT = process.env.PORT || 8080
 const HOST = "localhost"
 
 
@@ -10,10 +12,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    res.json({message: "API SETUP BY @https://github.com/lbrulet/APINIT"});
+    res.send({ message: "API SETUP BY @https://github.com/lbrulet/APINIT" });
 });
 
-app.listen(PORT, HOST, function (req, res) {
-    console.log("Server launched on " + HOST + ":" + PORT);
+app.get('/add', function (req, res) {
+    var user = new User();
+    user.username = "Luc Brulet";
+    user.password = "oui"
+    user.save(function (err) {
+        if (err)
+            res.send({ message: err })
+        res.send({ message: "Success" })
+    })
+})
+
+app.get('/get', function (req, res) {
+    User.find({}, function (err, data) {
+        if (err)
+            res.send(err)
+        res.send(data);
+    });
+})
+
+app.listen(PORT, function (req, res) {
+    console.log("Server launched");
 });
 
