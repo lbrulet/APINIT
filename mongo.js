1d005bc06ea365a92e562c15f6221b98a7b00542
sankamille <luc.brulet@epitech.eu>
2018-11-22 11:32:56 +0100

    [-] mongo.js deleted in root, moved to config folder

--- mongo.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://mongo:27017/users', { useNewUrlParser: true }, function (err) {
-    if (err) {
-        throw err;
-    }
-    console.log("Connected on mongodb");
-});
