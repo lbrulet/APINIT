e151479f5c44406a295358f21f81b022cd12ae7c
sankamille <luc.brulet@epitech.eu>
2018-11-26 21:23:20 +0100

    [~] Add console trace for docker logs

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -2,6 +2,8 @@ const express = require("express")
 const User = require('../../models/usersModels')
 const jwtConf = require('../../config/jwt')
 const jwt = require('jsonwebtoken')
+const colors = require("colors")
+const dateFormat = require("dateformat")
 const router = express.Router()
 
 router.post('/login', function (req, res) {
@@ -15,9 +17,10 @@ router.post('/login', function (req, res) {
                 return res.status(403).send({ message: err })
             if (!isMatch)
                 return res.status(403).send({ message: "Username or password does not exist!" })
-            var payload = { id: user.id };            
+            var payload = { id: user.id };
             var token = jwt.sign(payload, jwtConf.jwtOptions.secretOrKey);
-            console.log(process.env.SECRET)
+            var now = new Date();
+            console.log(colors.bold.green('[%s] - [ %s ] : connexion to %s'), dateFormat(now, "yyyy-mm-dd, HH:MM:ss"), req.body.user.username, req.originalUrl);
             res.status(200).send({ message: "Logged in with success!", token: token, client_id: user._id })
         })
     })
--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const User = require('../../models/usersModels')
+const colors = require("colors")
+const dateFormat = require("dateformat")
 const router = express.Router()
 
 router.put('/register', function (req, res) {
     if (!req.body.user.username || !req.body.user.password)
-    return res.status(403).send({ message: "Username or password is undefined!" })
+        return res.status(403).send({ message: "Username or password is undefined!" })
     User.findOne({ username: req.body.user.username }, function (err, user) {
         if (user)
             return res.status(403).send({ message: "Username already exist!" })
@@ -14,6 +16,8 @@ router.put('/register', function (req, res) {
         newUser.save(function (err) {
             if (err)
                 return res.status(403).send({ message: err })
+            var now = new Date();
+            console.log(colors.bold.green('[%s] - [ %s ] : connexion to %s'), dateFormat(now, "yyyy-mm-dd, HH:MM:ss"), req.body.user.username, req.originalUrl);
             res.status(200).send({ message: "User: " + req.body.user.username + " has been registered!" })
         })
     })
